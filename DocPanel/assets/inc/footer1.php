02d5735c76bc4581e940f1de512dd417356e09cf
Simon Kamangaru <63295027+SimonMunyiri@users.noreply.github.com>
2024-12-10 08:49:04 +0300

    Hospital Management System

---
+++ DocPanel/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt">
+     &copy; Copyright <?php echo date('Y'); ?> | All Rights Reserved | Hospital Management System | Developed By Simon Kamangaru Munyiri</a>
+</footer>
---
+++ header.php
@@ -0,0 +1,80 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Hospital Management System</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Hospital Management System" name="keywords">
+    <meta content="Hospital Management System" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">  
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <!-- Container to hold the header's data -->
+      <!-- Topbar Start -->
+    <div class="container-fluid py-2 border-bottom d-none d-lg-block">
+        <div class="container">
+            <div class="row">
+               
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-decoration-none text-body pe-3" href=""><i class="bi bi-telephone me-2"></i>+254 790 267919</a>
+                        <span class="text-body">|</span>
+                        <a class="text-decoration-none text-body px-3" href=""><i class="bi bi-envelope me-2"></i>munyirisimon6@gmail.com</a>
+                        <span class="text-body">|</span>
+                        <a class="text-decoration-none text-body px-3" href="AdminPanel\admin_login.php"><i class="bi bi-envelope me-2"></i>Admin Portal</a>
+                        <span class="text-body">|</span>
+                        <a class="text-decoration-none text-body px-3" href="DocPanel\docs_login.php"><i class="bi bi-envelope me-2"></i>Doctor's Portal</a>
+
+                        
+
+                    </div>
+               
+                
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+    <!-- Navbar Start -->
+    <div class="container-fluid sticky-top bg-white shadow-sm">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
+                <a href="index.html" class="navbar-brand">
+                    <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>CLINIC WEBSITE</h1>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.php" class="nav-item nav-link ">Home</a>
+                        <a href="about.php" class="nav-item nav-link">About</a>
+                        <a href="service.php" class="nav-item nav-link">Service</a>
+                        <a href="price.php" class="nav-item nav-link">Pricing</a>                       
+                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+    
+</html>
